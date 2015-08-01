fc0bea19441dcd485546d3b84219212f1b5fda62
tuoxie119 <liyashuang119@163.com>
2015-08-01 23:26:13 +0800

    new files

---
+++ spring-redis/src/main/java/com/leeyas/dao/AbstractBaseRedisDao.java
@@ -0,0 +1,30 @@
+package com.leeyas.dao;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.stereotype.Service;
+
+public abstract class AbstractBaseRedisDao<K, V> {  
+    
+    @Resource
+    protected RedisTemplate<K, V> redisTemplate;  
+  
+    /** 
+     * …Ë÷√redisTemplate 
+     * @param redisTemplate the redisTemplate to set 
+     */  
+    public void setRedisTemplate(RedisTemplate<K, V> redisTemplate) {  
+        this.redisTemplate = redisTemplate;  
+    }  
+      
+    /** 
+     * ªÒ»° RedisSerializer 
+     * <br>------------------------------<br> 
+     */  
+    protected RedisSerializer<String> getRedisSerializer() {  
+        return redisTemplate.getStringSerializer();  
+    }  
+}  
---
+++ spring-redis/src/main/java/com/leeyas/dao/IUserDao.java
@@ -0,0 +1,53 @@
+package com.leeyas.dao;
+
+import java.util.List;
+
+import com.leeyas.entity.User;
+
+public interface IUserDao {
+	 /** 
+     * 新增 
+     * <br>------------------------------<br> 
+     * @param user 
+     * @return 
+     */  
+    boolean add(User user);  
+      
+    /** 
+     * 批量新增 使用pipeline方式 
+     * <br>------------------------------<br> 
+     * @param list 
+     * @return 
+     */  
+    boolean add(List<User> list);  
+      
+    /** 
+     * 删除 
+     * <br>------------------------------<br> 
+     * @param key 
+     */  
+    void delete(String key);  
+      
+    /** 
+     * 删除多个 
+     * <br>------------------------------<br> 
+     * @param keys 
+     */  
+    void delete(List<String> keys);  
+      
+    /** 
+     * 修改 
+     * <br>------------------------------<br> 
+     * @param user 
+     * @return  
+     */  
+    boolean update(User user);  
+  
+    /** 
+     * 通过key获取 
+     * <br>------------------------------<br> 
+     * @param keyId 
+     * @return  
+     */  
+    User get(String keyId);  
+}
